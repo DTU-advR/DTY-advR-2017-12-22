9f4e649fcdb0d444b34e747eaf3dbb7428a71181
michbur <michalburdukiewicz@gmail.com>
2018-01-12 09:57:25 +0100

    updated examples

--- main.R
+++ main.R
@@ -1,34 +1,44 @@
 library(broom)
 
 set.seed(1459)
+x <- 1L:10 + rnorm(10)
+y <- 1L:10
 
 # htest objects -------------------------- 
-(cor_test <- cor.test(x = 1L:10 + rnorm(10), y = 1L:10))
+(cor_test <- cor.test(x = x, y = y))
 
 str(cor_test)
 
+# glance - a very brief summary
 glance(cor_test)
+
+
+wilcoxon_test <- wilcox.test(x = x, y = y)
+glance(wilcoxon_test)
 
 # lm objects ---------------------------------
 
-iris_model <- lm(Sepal.Length ~ Petal.Length + Species, data = iris)
+(iris_model <- lm(Sepal.Length ~ Petal.Length + Species, data = iris))
 
 str(iris_model) # no adjusted r^2
 summary(iris_model)
 str(summary(iris_model))
 
 glance(iris_model)
+
+# tidy - a model in a data.frame format (term-wise)
 tidy(iris_model)
+
+# augment - expand the input data using residuals and so on
 augment(iris_model)
 
 # aov objects ---------------------------------
-npk_model <- aov(yield ~ block + N * P + K, npk)
+(npk_model <- aov(yield ~ block + N * P + K, npk))
 
-glance(npk_model)
+summary(npk_model)
 tidy(npk_model)
-augment(npk_model)
 
-# kmeans objects
+augment(npk_model)
 
 
 # Stan objects
@@ -47,3 +57,4 @@ dim(summary(fit)[[2]])
 
 tidy(fit)
 
+# other packages: sweep: tidy data for time series: https://github.com/business-science/sweep 
